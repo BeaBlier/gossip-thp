d0735cbbbb36daaf6da12ed15ccc137bbf0366a9
BeaBlier <beatrice.blier@ieseg.fr>
2018-01-31 14:52:24 +0100

    2nd comiit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+
+  root 'gossips#home'
+#  get 'gossips/home', to: 'gossips#home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
